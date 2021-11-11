11b932faa540b240be1e8c4acaf3a4560e1678e4
Janjiran <jiranjan03@gmail.com>
2021-11-11 21:14:55 +0100

    Homepage created

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,21 +1,30 @@
 // (C) 2021 GoodData Corporation
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import classNames from "classnames/bind";
-import { DashboardView } from "@gooddata/sdk-ui-ext";
+// import { DashboardView } from "@gooddata/sdk-ui-ext";
 
 import styles from "./home.scss";
 
 import { Sidebar } from "~components/sidebar";
-import { DASHBOARD_ID } from "~constants";
+// import { DASHBOARD_ID, WORKSPACE_ID } from "~constants";
 
 const cx = classNames.bind(styles);
 
 export const Home: FC = () => {
+    const [reloadIframe, setReloadIframe] = useState(0);
     return (
         <div className={cx("home")}>
-            <Sidebar classNames={cx("sidebar")} />
+            <Sidebar
+                onSubmit={() => setReloadIframe(reloadIframe + 1)}
+                classNames={cx("sidebar")}
+            />
             <div className={cx("dashboard")}>
-                <DashboardView dashboard={DASHBOARD_ID} />
+                <iframe
+                    key={reloadIframe}
+                    src="https://internaltraining.na.gooddata.com/dashboards/embedded/#/workspace/nhf9oxqkkgvspwn0gl4d7ejm9z5c37gp/dashboard/acJWbpaMbQEb?showNavigation=true" // TODO: refactor
+                    width="100%"
+                    frameBorder="0"
+                />
             </div>
         </div>
     );
